962229fba790894f01ab06dbf88f44d193126fa4
Rafael Glowacki <149637380+rafaelglowack1@users.noreply.github.com>
2025-12-23 14:09:51 -0300

    Terminado a estilização do Investments

--- src/pages/Investments/index.tsx
+++ src/pages/Investments/index.tsx
@@ -1,5 +1,174 @@
+import { Container } from "../../styles"
+import { Card, CardBox, CardProfit, CardTag, CardTitle, CardTotalValue, CardValue, EncapsulateCardBox, NavBar, NavBarItem, NavBarSection } from "./style"
+
 const Investments = () => {
-    return <h1>Investments</h1>
+    return (
+        <Container>
+            <NavBar>
+                <NavBarSection>
+                    <NavBarItem>Todos</NavBarItem>
+                    <NavBarItem>Ações</NavBarItem>
+                    <NavBarItem>BDRs</NavBarItem>
+                    <NavBarItem>Fiis</NavBarItem>
+                </NavBarSection>
+                <NavBarItem>Adicionar Ativo</NavBarItem>
+            </NavBar>
+            <EncapsulateCardBox>
+                <CardBox>
+                    <Card>
+                        <h3>Nome</h3>
+                        <CardTag>ação</CardTag>
+                    </Card>
+                    <div>klabin</div>
+                    <Card>
+                        <CardTitle>Preço Médio</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Preço Atual</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Quantidade</CardTitle>
+                        <CardValue>500</CardValue>
+                    </Card>
+                    <CardTotalValue>
+                        <Card>
+                            <CardTitle>Valor Total</CardTitle>
+                            <CardValue>R$ 0,00</CardValue>
+                        </Card>
+                        <CardProfit>
+                            <p>34%</p>
+                            <p>+ R$ 600,00</p>
+                        </CardProfit>
+                    </CardTotalValue>
+                    <div>mais informações</div>
+                </CardBox>
+                <CardBox>
+                    <Card>
+                        <h3>Nome</h3>
+                        <CardTag>ação</CardTag>
+                    </Card>
+                    <div>klabin</div>
+                    <Card>
+                        <CardTitle>Preço Médio</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Preço Atual</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Quantidade</CardTitle>
+                        <CardValue>500</CardValue>
+                    </Card>
+                    <CardTotalValue>
+                        <Card>
+                            <CardTitle>Valor Total</CardTitle>
+                            <CardValue>R$ 0,00</CardValue>
+                        </Card>
+                        <CardProfit>
+                            <p>34%</p>
+                            <p>+ R$ 600,00</p>
+                        </CardProfit>
+                    </CardTotalValue>
+                    <div>mais informações</div>
+                </CardBox>
+                <CardBox>
+                    <Card>
+                        <h3>Nome</h3>
+                        <CardTag>ação</CardTag>
+                    </Card>
+                    <div>klabin</div>
+                    <Card>
+                        <CardTitle>Preço Médio</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Preço Atual</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Quantidade</CardTitle>
+                        <CardValue>500</CardValue>
+                    </Card>
+                    <CardTotalValue>
+                        <Card>
+                            <CardTitle>Valor Total</CardTitle>
+                            <CardValue>R$ 0,00</CardValue>
+                        </Card>
+                        <CardProfit>
+                            <p>34%</p>
+                            <p>+ R$ 600,00</p>
+                        </CardProfit>
+                    </CardTotalValue>
+                    <div>mais informações</div>
+                </CardBox>
+                <CardBox>
+                    <Card>
+                        <h3>Nome</h3>
+                        <CardTag>ação</CardTag>
+                    </Card>
+                    <div>klabin</div>
+                    <Card>
+                        <CardTitle>Preço Médio</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Preço Atual</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Quantidade</CardTitle>
+                        <CardValue>500</CardValue>
+                    </Card>
+                    <CardTotalValue>
+                        <Card>
+                            <CardTitle>Valor Total</CardTitle>
+                            <CardValue>R$ 0,00</CardValue>
+                        </Card>
+                        <CardProfit>
+                            <p>34%</p>
+                            <p>+ R$ 600,00</p>
+                        </CardProfit>
+                    </CardTotalValue>
+                    <div>mais informações</div>
+                </CardBox>
+                <CardBox>
+                    <Card>
+                        <h3>Nome</h3>
+                        <CardTag>ação</CardTag>
+                    </Card>
+                    <div>klabin</div>
+                    <Card>
+                        <CardTitle>Preço Médio</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Preço Atual</CardTitle>
+                        <CardValue>R$ 0,00</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Quantidade</CardTitle>
+                        <CardValue>500</CardValue>
+                    </Card>
+                    <CardTotalValue>
+                        <Card>
+                            <CardTitle>Valor Total</CardTitle>
+                            <CardValue>R$ 0,00</CardValue>
+                        </Card>
+                        <CardProfit>
+                            <p>34%</p>
+                            <p>+ R$ 600,00</p>
+                        </CardProfit>
+                    </CardTotalValue>
+                    <div>mais informações</div>
+                </CardBox>
+                
+            </EncapsulateCardBox>
+
+        </Container>
+    )
 }
 
 export default Investments
